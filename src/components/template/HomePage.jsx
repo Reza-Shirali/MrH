c32d0b7297ee32c979f20b675c7f1223db495bf5
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-09-15 19:03:15 +0330

    title product

--- src/components/template/HomePage.jsx
+++ src/components/template/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./HomePage.module.css";
+
 import Banner from "../modules/Banner.jsx";
 
 import { imageBanner } from "../../services/imageBanner.js";
@@ -8,6 +10,12 @@ function HomePage() {
   return (
     <>
       <Banner imageBanner={imageBanner} />
+      <div className={styles.container__title}>
+        <h2>
+          محصولات
+          <span className={styles.title}>محصولات</span>
+        </h2>
+      </div>
     </>
   );
 }
