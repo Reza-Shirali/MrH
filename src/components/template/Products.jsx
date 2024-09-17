334fd040b35c5458a706987c11ee178c54741f42
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-09-17 10:34:08 +0330

    product styles

--- src/components/template/Products.jsx
+++ src/components/template/Products.jsx
@@ -2,11 +2,13 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { REACT_APP_API_URL } from "../../services/apiData.js";
 
-import styles from "../template/styles.module.css"
+import styles from "../template/product.module.css";
+
+import { BsCart } from "react-icons/bs";
+import { FaRegEye } from "react-icons/fa";
 
 function ProductsPage() {
   const [products, setProducts] = useState([]);
-  // const apiUrl  = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
     axios.get(REACT_APP_API_URL).then((res) => {
@@ -14,26 +16,37 @@ function ProductsPage() {
     });
   }, []);
 
-
   return (
     <div className={styles.container}>
-      <h1>Products Page</h1>
-      <ul className={styles.list}>
-        {products.length > 0 ? (
-          products.map((product, index) => (
-            <li key={index} >
-              <img
-              className={styles.image__product_title}
-                src={product.images[0].image_link}
-                alt={product.name || "Product Image"}
-              />
-              <p>{product.name}</p>
-            </li>
-          ))
-        ) : (
-          <li>No products available</li>
-        )}
-      </ul>
+      <div className={styles.container__products}>
+        <ul className={styles.list}>
+          {products.length > 0 ? (
+            products.map((product, index) => (
+              <li key={index} className={styles.product}>
+                <img
+                  className={styles.image__product_title}
+                  src={product.images[0].image_link}
+                  alt={product.name || "Product Image"}
+                />
+                <p>{product.name}</p>
+                <div className={styles.product__info}>
+                  <div className={styles.add__product}>
+                    <BsCart />
+                  </div>
+                  <div className={styles.show__product}>
+                    <FaRegEye />
+                  </div>
+                  <div className={styles.product__price}>
+                    5000
+                  </div>
+                </div>
+              </li>
+            ))
+          ) : (
+            <li>No products available</li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
