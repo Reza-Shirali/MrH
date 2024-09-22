b05fc86262483eb1b7ce4c9c6cb7ff347ef7338d
RezaShirali <shir.ez.aliofficial@gmail.com>
2024-09-22 09:32:08 +0330

    finish jsx shopping Cart

--- src/components/template/ShoppingCart.jsx
+++ src/components/template/ShoppingCart.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import styles from "./ShoppingCart.module.css";
+
 function ShoppingCart() {
-  return <div>ShoppingCart</div>;
+  return (
+    <>
+      <div className={styles.container__shopping_cart}>
+        <h3>سبد خرید</h3>
+        <div className={styles.wrapper__itemCart}>
+          <div className={styles.wrapper__product_cart}></div>
+          <div className={styles.wrapper__total_price}></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ShoppingCart;
